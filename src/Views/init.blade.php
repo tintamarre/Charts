534878aa23839972de43aa062ba9de864eb66eb9
Martin Erpicum <martin@erpicum.net>
2018-12-05 16:32:39 +0100

    Load labels/datasets dynamically

--- src/Views/init.blade.php
+++ src/Views/init.blade.php
@@ -2,11 +2,13 @@ let {{ $chart->id }}_rendered = false;
 let {{ $chart->id }}_load = function () {
     if (document.getElementById("{{ $chart->id }}") && !{{ $chart->id }}_rendered) {
         @if ($chart->api_url)
-            fetch("{!! $chart->api_url !!}")
-                .then(data => data.json())
-                .then(data => { {{ $chart->id }}_create(data) });
+        $.get("{!! $chart->api_url !!}", function(response){
+          labels = response.labels;
+          datasets = response.datasets;
+          {{ $chart->id }}_create(datasets);
+       });
         @else
-            {{ $chart->id }}_create({!! $chart->formatDatasets() !!})
+        {{ $chart->id }}_create({!! $chart->formatDatasets() !!})
         @endif
     }
 };
