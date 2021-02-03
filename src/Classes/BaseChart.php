96d3f980f2015f5df6d5822f5613c6be17e30dee
Martin Erpicum <martin@erpicum.net>
2021-02-03 15:11:47 +0100

    fix updatebase chart

--- src/Classes/BaseChart.php
+++ src/Classes/BaseChart.php
@@ -185,10 +185,7 @@ class BaseChart
      */
     public function options($options, bool $overwrite = false)
     {
-        if (!empty($options['plugins'])) {
-            $options['plugins'] = new Raw(trim(preg_replace('/\s\s+/', ' ', $options['plugins'])));
-        }
-
+    
         if ($options instanceof Collection) {
             $options = $options->toArray();
         }
